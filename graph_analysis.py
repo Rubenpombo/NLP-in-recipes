459e70d60627fdcbc8a8896a6e4f800112badc17
Rubén Parte Pombo <146735955+Rubenpombo@users.noreply.github.com>
2024-07-25 10:57:33 +0200

    Add files via upload

--- graph_analysis.py
+++ graph_analysis.py
@@ -12,7 +12,7 @@ for ingrediente1, counts in relaciones.items():
 
 # [ES] Algoritmo layout para posicionar los nodos, que calcula la posición de los nodos de manera que los nodos conectados con mayor peso estén más cerca entre sí
 # [EN] Layout algorithm to position the nodes, which calculates the arrangement of the nodes so that the nodes connected with greater weight are closer to each other
-pos = nx.spring_layout(G, seed=3)
+pos = nx.spring_layout(G, seed=3, k=0.6)
 nx.draw(G,pos,with_labels=True,node_color='khaki', node_size=1000, font_size=9, font_color='black')
 
 # [ES] Los resultados se guardan en la carpeta outputs
@@ -20,9 +20,29 @@ nx.draw(G,pos,with_labels=True,node_color='khaki', node_size=1000, font_size=9, 
 import os
 output_folder = "outputs"
 os.makedirs(output_folder, exist_ok=True)  
-output_path = os.path.join(output_folder, "grafo.png")
+output_path = os.path.join(output_folder, "graph.png")
 plt.savefig(output_path)
 plt.close() 
+
+
+
+# [ES] Analisis de las conexiones más importantes del grafo
+# [EN] Analysis of the most important connections in the graph
+G = nx.Graph()
+for ingrediente1, counts in relaciones.items():
+    for ingrediente2, count in counts.items():
+        if ingrediente1 != ingrediente2 and count > 2:
+            G.add_edge(ingrediente1, ingrediente2, weight=count)
+
+pos = nx.spring_layout(G, seed=3, k=0.6)
+nx.draw(G,pos,with_labels=True,node_color='khaki', node_size=1000, font_size=9, font_color='black')
+nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G, 'weight'))
+
+output_path = os.path.join(output_folder, "important_connections.png")
+plt.savefig(output_path)
+plt.close() 
+
+
 
 # [ES] Centralidad de grado
 # [EN] Degree centrality
@@ -42,6 +62,8 @@ closeness = nx.closeness_centrality(G)
 print("\n Closeness centrality:\n")
 print(closeness)
 
+
+
 # [ES] Algoritmo de detección de comunidades Louvain
 # [EN] Louvain community detection
 from community import community_louvain
@@ -49,7 +71,6 @@ comunidades = community_louvain.best_partition(G)
 colores_nodos = [comunidades[node] for node in G.nodes]
 nx.draw(G, pos, with_labels=True, node_color=colores_nodos, node_size=1000, font_size=9, font_color='black')
 
-
-output_path_comunidades = os.path.join(output_folder, "grafo_comunidades.png")
+output_path_comunidades = os.path.join(output_folder, "communities.png")
 plt.savefig(output_path_comunidades)
 plt.close() 
